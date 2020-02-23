46548accb61f8a0966d17d0c89fdbe7e762dab34
Krstan Vjestica <krle@tilda.center>
2020-02-23 15:44:03 +0100

    create vagrant box creator FreeBSD

---
+++ README.md
@@ -0,0 +1,126 @@
+# bsdbox
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**
+
+- [About](#about)
+- [Install FreeBSD root on ZFS](#install-freebsd-root-on-zfs)
+- [Virtualbox Settings](#virtualbox-settings)
+- [Package for Vagrant](#package-for-vagrant)
+- [Setup](#setup)
+- [License](#license)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## About
+
+Vagrant box with latest FreeBSD and ZFS.
+
+## Install FreeBSD root on ZFS
+
+1) Run
+
+```sh
+su -
+```
+to get a root shell.
+
+2) Run DHCP client by running
+
+```sh
+dhclient vtnet0
+```
+to get an IPv4 address.
+
+3) We can now start an SSH daemon by running:
+
+```sh
+mkdir /tmp/etc
+mount_unionfs /tmp/etc /etc
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+passwd root
+service sshd onestart
+```
+
+4) Login to the FreeBSD Shell by running:
+
+```sh
+ssh root@<ip_address>
+```
+
+5)
+
+```sh
+cd /tmp
+# download zfsinstall.sh from github
+fetch --no-verify-peer https://raw.github.com/krlex/bsdbox/master/bin/zfsinstall.sh
+chmod 750 zfsinstall.sh
+./zfsinstall.sh -d ada0 -p zroot -r 8GB -s 2GB
+```
+
+```sh
+shutdown -h now
+```
+
+```sh
+cd /tmp
+# download vagrant-setup.sh from github
+fetch --no-verify-peer https://raw.github.com/krlex/bsdbox/master/bin/vagrant-setup.sh
+chmod 750 vagrant-setup.sh
+./vagrant-setup.sh
+```
+
+## Virtualbox Settings
+
+Create a new Virtual Machine with the following settings:
+
+- System -> Motherboard -> Enable I/O APIC
+- System -> Motherboard -> Hardware clock in UTC time
+- System -> Processor -> 2 CPUs
+- System -> Acceleration -> Enable VT-x/AMD-V
+- System -> Acceleration -> Enable Nested Paging
+- Storage -> Add SATA Controller
+- Storage -> Attach a .vdi disk to SATA Port 0
+- Audio -> Disable Audio
+- Network -> Adapter 1 -> Attached to: NAT
+- Network -> Adapter 1 -> Advanced -> Adapter Type -> Paravirtualized Network (virtio-net)
+- Network -> Adapter 2 -> Attached to: host-only Adapter
+- Network -> Adapter 2 -> Advanced -> Adapter Type -> Paravirtualized Network (virtio-net)
+- Ports -> Disable Serial Port
+- Ports -> Disable USB Controller
+
+## Package for Vagrant
+
+```
+vagrant package --base <name-of-your-virtual-machine> --output freebsd-12.1-amd64.box
+```
+
+## Setup
+
+1) Install dependencies
+
+* [Vagrant](https://www.vagrantup.com) 2.2.7 or greater.
+* [VirtualBox](https://www.virtualbox.org) 5.2.10 or greater.
+
+2) Clone this project.
+
+```
+git clone https://github.com/krlex/bsdbox.git
+cd bsdbox
+```
+
+3) Install vagrant plugins
+
+```
+vagrant plugin install vagrant-cachier
+vagrant plugin install vagrant-hostsupdater
+vagrant plugin install vagrant-vbguest
+```
+
+4) Startup and SSH
+
+```
+vagrant up
+vagrant ssh
+```
---
+++ bin/zfsinstall.sh
@@ -0,0 +1,208 @@
+#!/usr/bin/env sh
+#
+# ZFS root install script
+#
+# Copyright (c) 2020 Krstan Vjestica <krle[at]tilda[dot]center>
+
+set -e
+# set -x
+
+BOOTSIZE=512k
+DEFAULT_POOLNAME=zroot
+DEFAULT_ZFSROOTSIZE=10GB
+DEFAULT_SWAPSIZE=2GB
+
+usage() {
+  echo "Usage: $0 -d device [-h] [-p pool_name] [-r zfs_root_size] [-s swap_size] [-l l2arc_size] [-z zil_size]"
+}
+
+while getopts d:h:p:r:s:l:z: o; do
+  case "$o" in
+    d)
+      DEVICE="${OPTARG}"
+      ;;
+    h)
+      usage;
+      exit 1
+      ;;
+    p)
+      POOL="${OPTARG}"
+      ;;
+    r)
+      ZFSROOTSIZE="${OPTARG}"
+      ;;
+    s)
+      SWAPSIZE="${OPTARG}"
+      ;;
+    l)
+      L2ARCSIZE="${OPTARG}"
+      ;;
+    z)
+      ZILSIZE="${OPTARG}"
+      ;;
+    [?])
+      usage;
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$DEVICE" ]; then
+  usage
+  exit 1
+fi
+
+if [ -z "$POOL" ]; then
+  POOL=${DEFAULT_POOLNAME}
+fi
+
+if [ -z "$ZFSROOTSIZE" ]; then
+  ZFSROOTSIZE=${DEFAULT_ZFSROOTSIZE}
+fi
+
+if [ -z "$SWAPSIZE" ]; then
+  SWAPSIZE=${DEFAULT_SWAPSIZE}
+fi
+
+#
+# Disk Partitioning Root
+#
+
+# delete a GUID partition table on boot disk (the SSD)
+gpart destroy -F ${DEVICE} > /dev/null 2>&1 || true
+zpool labelclear -f ${DEVICE} > /dev/null 2>&1 || true
+# get rid of any MBR
+dd if=/dev/zero of=/dev/${DEVICE} bs=1m count=1 >&2
+
+# create a GUID partition table on boot disk (the SSD)
+gpart create -s gpt ${DEVICE}
+
+# create necessary partitions on root disk and add ZFS aware boot code
+
+# add the boot partition
+gpart add -l boot -t freebsd-boot -a 4k -s ${BOOTSIZE} ${DEVICE}
+# add the swap partition
+gpart add -l swap -t freebsd-swap -a 4k -s ${SWAPSIZE} ${DEVICE}
+# add the ZFS partition
+gpart add -l system -t freebsd-zfs -a 4k -s ${ZFSROOTSIZE} ${DEVICE}
+# write boot loader to the disk
+gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 ${DEVICE}
+
+# create ZIL and L2ARC partitions
+if [ -n "${ZILSIZE}" -a -n "${L2ARCSIZE}" ]; then
+  gpart add -l log -t freebsd-zfs -a 4k -s ${ZILSIZE} ${DEVICE}
+  gpart add -l cache -t freebsd-zfs -a 4k -s ${L2ARCSIZE} ${DEVICE}
+fi
+
+# create virtual device which define 4K sector size
+gnop create -S 4096 /dev/gpt/system
+
+# load ZFS module
+kldload zfs
+
+# create zpool
+zpool create -f -m none -o altroot=/mnt -O canmount=off ${POOL} /dev/gpt/system.nop
+
+#
+# Create datasets based off 10.1-RELEASE layout
+#
+
+zfs set checksum=fletcher4 ${POOL}
+zfs set dedup=off ${POOL}
+
+zfs create -o compression=lz4 -o atime=off -o mountpoint=none ${POOL}/ROOT
+zfs create -o compression=lz4 -o atime=off -o mountpoint=/ ${POOL}/ROOT/default
+zfs create -o compression=lz4 -o atime=off -o mountpoint=/tmp -o exec=on -o setuid=off ${POOL}/tmp
+zfs create -o compression=lz4 -o atime=off -o mountpoint=/usr -o canmount=off ${POOL}/usr
+zfs create -o compression=lz4 -o atime=off ${POOL}/usr/home
+zfs create -o compression=lz4 -o atime=off ${POOL}/usr/obj
+zfs create -o compression=lz4 -o atime=off -o setuid=off ${POOL}/usr/ports
+zfs create -o compression=lz4 -o atime=off ${POOL}/usr/src
+zfs create -o compression=lz4 -o atime=off -o mountpoint=/var -o canmount=off ${POOL}/var
+zfs create -o compression=lz4 -o atime=off -o exec=off -o setuid=off ${POOL}/var/audit
+zfs create -o compression=lz4 -o atime=off -o exec=off -o setuid=off ${POOL}/var/crash
+zfs create -o compression=lz4 -o atime=off -o exec=off -o setuid=off ${POOL}/var/log
+zfs create -o compression=lz4 -o atime=on ${POOL}/var/mail
+zfs create -o compression=lz4 -o atime=off -o setuid=off ${POOL}/var/tmp
+
+chmod 1777 /mnt/tmp
+chmod 1777 /mnt/var/tmp
+
+zpool set bootfs=${POOL}/ROOT/default ${POOL}
+
+# export the zpool
+zpool export ${POOL}
+# detroy the gnops
+gnop destroy /dev/gpt/system.nop
+# re-import the zpool; look for devices in /dev/gpt to keep labels
+zpool import -d /dev/gpt -o altroot=/mnt ${POOL}
+
+# install FreeBSD
+cd /usr/freebsd-dist/
+sh -c 'for file in base.txz kernel.txz; do tar --unlink -xpJf $file -C /mnt; done'
+
+cat << EOF > /mnt/boot/loader.conf
+# /boot/loader.conf
+
+# load zfs
+zfs_load="YES"
+vfs.root.mountfrom="zfs:zroot/ROOT/default"
+
+# file system labels
+geom_label_load="YES"
+# use gpt labels instead of gpt uuids
+kern.geom.label.gptid.enable=0
+kern.geom.label.gpt.enable=1
+EOF
+
+cat << EOF > /mnt/etc/fstab
+#/etc/fstab
+
+# Device          Mountpoint    FStype    Options   Dump  Pass#
+/dev/gpt/swap     none          swap      sw        0     0
+EOF
+
+cat << EOF > /mnt/etc/rc.conf
+# /etc/rc.conf
+
+# power savings
+powerd_enable="NO"
+
+# ZFS support
+zfs_enable="YES"
+
+# hostname
+hostname="bsdbox"
+
+# network configuration
+ifconfig_vtnet0_name="bge0"
+ifconfig_vtnet1_name="bge1"
+ifconfig_bge0="SYNCDHCP"
+ifconfig_bge1="SYNCDHCP"
+
+# disable sendmail
+sendmail_enable="NO"
+sendmail_submit_enable="NO"
+sendmail_outbound_enable="NO"
+sendmail_msp_queue_enable="NO"
+
+# disable RPC Binder Daemon
+rpcbind_enable="NO"
+
+# syslog shouldn't listen for incoming connections
+syslogd_flags="-ss"
+
+# enable SSH Daemon
+sshd_enable="YES"
+
+# set keymap
+keymap="german.iso.kbd"
+
+# clear /tmp
+clear_tmp_enable="YES"
+
+# disable dumpdev
+dumpdev="NO"
+EOF
+
+mount -t devfs devfs /dev
